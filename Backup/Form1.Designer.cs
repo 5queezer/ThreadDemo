28742f06ddcd684cc8e7f18530009456901bd119
Christian Pojoni <34570565+5queezer@users.noreply.github.com>
2018-03-20 01:59:28 +0100

    Add files via upload

---
+++ Backup/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace ThreadDemo
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Erforderliche Designervariable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Verwendete Ressourcen bereinigen.
+		/// </summary>
+		/// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+		protected override void Dispose( bool disposing )
+		{
+			if( disposing && (components != null) )
+			{
+				components.Dispose();
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Vom Windows Form-Designer generierter Code
+
+		/// <summary>
+		/// Erforderliche Methode für die Designerunterstützung.
+		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtMessage = new System.Windows.Forms.TextBox();
+			this.pgbState = new System.Windows.Forms.ProgressBar();
+			this.lbData = new System.Windows.Forms.ListBox();
+			this.btnStart = new System.Windows.Forms.Button();
+			this.btnClose = new System.Windows.Forms.Button();
+			this.btnCancel = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// txtMessage
+			// 
+			this.txtMessage.Location = new System.Drawing.Point( 12, 12 );
+			this.txtMessage.Name = "txtMessage";
+			this.txtMessage.Size = new System.Drawing.Size( 254, 20 );
+			this.txtMessage.TabIndex = 0;
+			// 
+			// pgbState
+			// 
+			this.pgbState.Location = new System.Drawing.Point( 12, 38 );
+			this.pgbState.Name = "pgbState";
+			this.pgbState.Size = new System.Drawing.Size( 254, 23 );
+			this.pgbState.TabIndex = 1;
+			// 
+			// lbData
+			// 
+			this.lbData.FormattingEnabled = true;
+			this.lbData.Location = new System.Drawing.Point( 12, 82 );
+			this.lbData.Name = "lbData";
+			this.lbData.Size = new System.Drawing.Size( 254, 186 );
+			this.lbData.TabIndex = 2;
+			// 
+			// btnStart
+			// 
+			this.btnStart.Location = new System.Drawing.Point( 191, 301 );
+			this.btnStart.Name = "btnStart";
+			this.btnStart.Size = new System.Drawing.Size( 75, 23 );
+			this.btnStart.TabIndex = 3;
+			this.btnStart.Text = "Starten";
+			this.btnStart.UseVisualStyleBackColor = true;
+			this.btnStart.Click += new System.EventHandler( this.btnStart_Click );
+			// 
+			// btnClose
+			// 
+			this.btnClose.Location = new System.Drawing.Point( 12, 301 );
+			this.btnClose.Name = "btnClose";
+			this.btnClose.Size = new System.Drawing.Size( 75, 23 );
+			this.btnClose.TabIndex = 4;
+			this.btnClose.Text = "Schließen";
+			this.btnClose.UseVisualStyleBackColor = true;
+			this.btnClose.Click += new System.EventHandler( this.btnClose_Click );
+			// 
+			// btnCancel
+			// 
+			this.btnCancel.Location = new System.Drawing.Point( 110, 301 );
+			this.btnCancel.Name = "btnCancel";
+			this.btnCancel.Size = new System.Drawing.Size( 75, 23 );
+			this.btnCancel.TabIndex = 5;
+			this.btnCancel.Text = "Abbrechen";
+			this.btnCancel.UseVisualStyleBackColor = true;
+			this.btnCancel.Click += new System.EventHandler( this.btnCancel_Click );
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF( 6F, 13F );
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size( 278, 336 );
+			this.Controls.Add( this.btnCancel );
+			this.Controls.Add( this.btnClose );
+			this.Controls.Add( this.btnStart );
+			this.Controls.Add( this.lbData );
+			this.Controls.Add( this.pgbState );
+			this.Controls.Add( this.txtMessage );
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler( this.Form1_Load );
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler( this.Form1_FormClosing );
+			this.ResumeLayout( false );
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox txtMessage;
+		private System.Windows.Forms.ProgressBar pgbState;
+		private System.Windows.Forms.ListBox lbData;
+		private System.Windows.Forms.Button btnStart;
+		private System.Windows.Forms.Button btnClose;
+		private System.Windows.Forms.Button btnCancel;
+	}
+}
+
---
+++ Backup/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ThreadDemo
+{
+	public partial class Form1 : Form
+	{
+		public delegate void DelegateSetTextValue( string strVal );
+		public delegate void DelegateSetProgBarValue( int nVal );
+		public delegate void DelegateAddListBoxValue( string strVal );
+		public delegate void DelegateResetTextValue();
+		public delegate void DelegateResetProgBar();
+		public delegate void DelegateResetListBox();
+
+		private Thread m_thrWorker = null;
+		private Byte m_bytState = 0;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load( object sender, EventArgs e )
+		{
+			this.pgbState.Minimum = 0;
+			this.pgbState.Maximum = 100;
+		}
+
+		private void btnStart_Click( object sender, EventArgs e )
+		{
+			if( this.m_thrWorker != null )
+				return;
+
+			this.m_thrWorker = new Thread( new ParameterizedThreadStart( WorkingInBackground ) );
+			Thread.VolatileWrite( ref m_bytState, 1 );
+			this.m_thrWorker.Start( this );
+		}
+
+		private void btnCancel_Click( object sender, EventArgs e )
+		{
+			this.CloseThread();
+		}
+
+		private void btnClose_Click( object sender, EventArgs e )
+		{
+			this.Close();
+		}
+
+		private void Form1_FormClosing( object sender, FormClosingEventArgs e )
+		{
+			this.CloseThread();
+		}
+
+		private void CloseThread()
+		{
+			if( this.m_thrWorker != null )
+			{
+				Thread.VolatileWrite( ref m_bytState, 0 );
+				int nCount = 0;
+				while( this.m_thrWorker.IsAlive == true && nCount++ < 50 )
+					Thread.Sleep( 100 );
+
+				if( this.m_thrWorker.IsAlive == true )
+					this.m_thrWorker.Abort();
+				this.m_thrWorker = null;
+			}
+		}
+
+		public void WorkingInBackground( object obj )
+		{
+			Form1 frm = (Form1)obj;
+			int nCount = 1;
+			while( Thread.VolatileRead( ref m_bytState ) == 1 )
+			{
+				frm.SetTextValue( "Durchlauf: " + nCount.ToString() );
+				frm.SetProgBarValue( nCount );
+				frm.AddListBoxValue( nCount.ToString() );
+
+				if( nCount++ == 100 )
+				{
+					Thread.Sleep( 3000 );
+					frm.ResetTextValue();
+					frm.ResetProgBar();
+					frm.ResetListBox();
+					nCount = 1;
+				}
+				Thread.Sleep( 50 );
+
+			}
+		}
+
+		public void SetTextValue( string strVal )
+		{
+			if( this.txtMessage.InvokeRequired )
+				this.txtMessage.Invoke( new DelegateSetTextValue( SetTextValue ), new object[] { strVal } );
+			else
+				this.txtMessage.Text = strVal;
+		}
+
+		public void SetProgBarValue( int nVal )
+		{
+			if( this.pgbState.InvokeRequired )
+				this.pgbState.Invoke( new DelegateSetProgBarValue( SetProgBarValue ), new object[] { nVal } );
+			else
+				this.pgbState.Value = nVal;
+		}
+
+		public void AddListBoxValue( string strVal )
+		{
+			if( this.lbData.InvokeRequired )
+				this.lbData.Invoke( new DelegateAddListBoxValue( AddListBoxValue ), new object[] { strVal } );
+			else
+				this.lbData.Items.Add( strVal );
+		}
+
+		public void ResetTextValue()
+		{
+			if( this.txtMessage.InvokeRequired )
+				this.txtMessage.Invoke( new DelegateResetTextValue( ResetTextValue ) );
+			else
+				this.txtMessage.Text = "";
+		}
+
+		public void ResetProgBar()
+		{
+			if( this.pgbState.InvokeRequired )
+				this.pgbState.Invoke( new DelegateResetProgBar( ResetProgBar ) );
+			else
+				this.pgbState.Value = 0;
+		}
+
+		public void ResetListBox()
+		{
+			if( this.lbData.InvokeRequired )
+				this.lbData.Invoke( new DelegateResetListBox( ResetListBox ) );
+			else
+				this.lbData.Items.Clear();
+		}
+
+	}
+}
