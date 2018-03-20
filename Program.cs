7c4b43fd08b946ff554a3c8a59e5d526c9e93542
Christian Pojoni <34570565+5queezer@users.noreply.github.com>
2018-03-20 02:02:20 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ThreadDemo
+{
+	static class Program
+	{
+		/// <summary>
+		/// Der Haupteinstiegspunkt für die Anwendung.
+		/// </summary>
+		//[STAThread]
+        [MTAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault( false );
+			Application.Run( new Form1() );
+		}
+	}
+}
